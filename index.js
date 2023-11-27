f97fb19521d2baa467b2efe5f2c5a1d8314db8bf
Willy Silva <willy.silva@hellosaks.com>
2023-11-27 14:58:30 -0300

    add disconnect

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ class OpenVPNConnector {
     constructor(configFile) {
         this.configFile = configFile;
         this.credentialsFile = credentialsFile;
+        this.openvpnProcess;
     }
 
     connect() {
-        const openvpnProcess = exec(`openvpn --config ${this.configFile} --auth-user-pass ${credentialsFile}`, (error, stdout, stderr) => {
+        this.openvpnProcess = exec(`openvpn --config ${this.configFile} --auth-user-pass ${credentialsFile}`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error: ${error.message}`);
                 return;
@@ -19,23 +20,29 @@ class OpenVPNConnector {
             console.log(`OpenVPN stdout: ${stdout}`);
         });
 
-        openvpnProcess.stdout.on('data', (data) => {
+        this.openvpnProcess.stdout.on('data', (data) => {
             console.log(`OpenVPN data: ${data}`);
             // You can handle the output here if needed
         });
 
-        openvpnProcess.on('close', (code) => {
+        this.openvpnProcess.on('close', (code) => {
             console.log(`OpenVPN process exited with code ${code}`);
             // Handle exit code if necessary
         });
 
-        openvpnProcess.on('error', (err) => {
+        this.openvpnProcess.on('error', (err) => {
             console.error(`OpenVPN process error: ${err}`);
             // Handle errors if necessary
         });
     }
 
     disconnect() {
+        if (this.openvpnProcess) {
+            this.openvpnProcess.kill('SIGINT'); // Sending SIGINT signal to terminate the process
+            this.openvpnProcess = null;
+        } else {
+            console.log('OpenVPN process is not running.');
+        }
     }
 }
 
